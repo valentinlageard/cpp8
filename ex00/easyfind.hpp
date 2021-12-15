cca68ceb8d7e7733605445b8d018928aaaa94c0a
Valentin Lageard <valentin.lageard.pro@gmail.com>
2021-12-15 17:50:34 +0100

    adds new pdf; easyfind uses std::find

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,19 +3,17 @@
 
 # include <iterator>
 # include <stdexcept>
+# include <algorithm>
 
 template <typename T>
 int const & easyfind(T container, int n) {
 	typename T::iterator it;
 
-	it = container.begin();
-	while (it != container.end()) {
-		if (*it == n) {
-			return *it;
-		}
-		++it;
+	it = std::find(container.begin(), container.end(), n);
+	if (it == container.end()) {
+		throw std::runtime_error("value wasn't found");
 	}
-	throw std::runtime_error("value wasn't found");
+	return *it;
 }
 
 #endif
